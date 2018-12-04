2ef5ad795481699932cb81d4046a7a6ec811ef47
hongchuanwang <Hcw960810>
2018-12-04 15:10:17 +0800

    [update] gateway config

--- config-server/src/main/java/cn/edu/nju/ConfigServerApplication.java
+++ config-server/src/main/java/cn/edu/nju/ConfigServerApplication.java
@@ -3,12 +3,14 @@ package cn.edu.nju;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author hongchuanwang
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
