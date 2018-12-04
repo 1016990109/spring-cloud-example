07f3e7421391d4feecf3f938ec6e310476c9fd67
hongchuanwang <Hcw960810>
2018-12-04 14:31:11 +0800

    [rebuild] project

---
+++ config-server/src/main/java/cn/edu/nju/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package cn.edu.nju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author hongchuanwang
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
